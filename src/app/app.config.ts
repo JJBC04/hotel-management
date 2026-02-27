afbccf65cdf4f0829a075ca353d8c205af4c346a
JJBC04 <04juanjoseb@gmail.com>
2026-02-27 18:00:46 -0500

    Fix error en importar animaciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { mockApiInterceptor } from './core/interceptors/mock-api.interceptor';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([mockApiInterceptor]) // Registro del interceptor
     ),
